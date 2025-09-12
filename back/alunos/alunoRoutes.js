b0a87fda327af8d75f8aaaa2f4629d8206a33018
Nikolas <nikolasrailanmp@gmail.com>
2025-09-11 21:15:31 -0300

    editando outras infos de aluno e prof

--- back/alunos/alunoRoutes.js
+++ back/alunos/alunoRoutes.js
@@ -3,9 +3,16 @@ const router = express.Router();
 const alunoController = require("../controllers/alunoController");
 const { authenticateToken, isAdmin } = require("../middleware/authToken");
 
-router.get("/", alunoController.listarAlunos);
-router.post("/", alunoController.criarAluno);
+// Rota para criar um perfil de aluno (protegida para admin)
+router.post("/", authenticateToken, isAdmin, alunoController.criarAluno);
 
-router.patch("/:id", authenticateToken, alunoController.atualizarAluno);
-router.delete("/:id", authenticateToken, alunoController.deletarAluno);
+// Rota para listar todos os alunos (protegida para admin)
+router.get("/", authenticateToken, isAdmin, alunoController.listarAlunos);
+
+// Rota para deletar um perfil de aluno (protegida para admin)
+router.delete("/:id", authenticateToken, isAdmin, alunoController.deletarAluno);
+
+// A rota de atualização foi centralizada em /usuarios/:id e esta pode ser removida
+// router.patch("/:id", authenticateToken, alunoController.atualizarAluno);
+
 module.exports = router;
--- back/professores/professorRoutes.js
+++ back/professores/professorRoutes.js
@@ -3,10 +3,31 @@ const router = express.Router();
 const professorController = require("../controllers/professorController");
 const { authenticateToken, isAdmin } = require("../middleware/authToken");
 
-router.get("/", professorController.listarProfessores);
-router.post("/", professorController.criarProfessor);
+// Rota para criar um perfil de professor (geralmente usada pelo admin)
+router.post(
+  "/",
+  authenticateToken,
+  isAdmin,
+  professorController.criarProfessor
+);
 
-router.patch("/:id", authenticateToken, professorController.atualizarProfessor);
-router.delete("/:id", authenticateToken, professorController.deletarProfessor);
+// Rota para listar todos os professores (protegida para admin)
+router.get(
+  "/",
+  authenticateToken,
+  isAdmin,
+  professorController.listarProfessores
+);
+
+// Rota para deletar um perfil de professor (protegida para admin)
+router.delete(
+  "/:id",
+  authenticateToken,
+  isAdmin,
+  professorController.deletarProfessor
+);
+
+// A rota de atualização foi centralizada em /usuarios/:id e esta pode ser removida
+// router.patch("/:id", authenticateToken, professorController.atualizarProfessor);
 
 module.exports = router;
