ba35f4d08529c3487aa39eaf9731960a043bd4b9
Nikolas <nikolasrailanmp@gmail.com>
2025-10-16 10:20:43 -0300

    ajustando global.css

--- front/src/app/components/orientacao/OrientacaoTabs.js
+++ front/src/app/components/orientacao/OrientacaoTabs.js
@@ -1,6 +1,7 @@
 "use client";
 import React from "react";
 import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 
 const truncate = (str, n) => {
   return str.length > n ? str.substr(0, n - 1) + "..." : str;
@@ -12,9 +13,12 @@ const OrientacaoTabs = ({ orientacoes, selectedIndex, onSelect }) => {
       {orientacoes.map((orientacao, index) => (
         <Button
           key={orientacao.id_orientacao}
-          variant={selectedIndex === index ? "secondary" : "ghost"}
+          variant="ghost"
           onClick={() => onSelect(index)}
-          className="h-auto"
+          className={cn(
+            "h-auto",
+            selectedIndex === index && "bg-secondary text-secondary-foreground"
+          )}
         >
           <div className="flex flex-col items-start p-1 text-left">
             <span className="font-medium">
